46f40d3d865a8695b952652f8f02d0575afa619d
jothirathnam250179-dot <jothirathnam.250179@cse.ritchennai.edu.in>
2026-09-09 19:48:49 +0530

    Create Layout.js

---
+++ Layout.js
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'Smart Traffic Management',description:'Multi-factor traffic decision support dashboard'}
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
